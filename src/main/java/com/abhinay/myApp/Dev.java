72d04b77adb61fff4bd5f9d2104867b646f615fa
Abhinay Verma <aman.ver56@gmail.com>
2024-10-04 22:30:21 +0530

    Created spring project

---
+++ src/main/java/com/abhinay/myApp/Dev.java
@@ -0,0 +1,14 @@
+package com.abhinay.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+	
+	public void build(){
+		System.out.println("Learning Spring Boot");
+	}
+	
+	
+
+}
